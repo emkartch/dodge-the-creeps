b7cb633c9ffa89a5dcdd22d63f74489cfa481e5b
emkartch <elka9063@colorado.edu>
2025-09-07 16:02:02 -0600

    Dynamic Collision Sizes
    Made it so that the collision size would change depending on the animation of the mob.

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 This is my game for the "Your First Game" assignment in ATLS 4140.
 
 9/3 - I started by following the [Your First 2D Game](https://docs.godotengine.org/en/stable/getting_started/first_2d_game/index.html) tutorial in the GoDot Docs (which included using their provided assets) and it took me about 3 hours to complete.
+
+9/7 - **First Change**: Dynamic Collision Size - 1 hour 30 minutes
+When I finished the tutorial I noticed that, despite the different sizes of the enemies, the collision size stayed consistent. I have added code to make the collision size fit the animation.
--- mob.gd
+++ mob.gd
@@ -2,8 +2,17 @@ extends RigidBody2D
 
 func _ready():
 	var mob_types = Array($AnimatedSprite2D.sprite_frames.get_animation_names())
-	$AnimatedSprite2D.animation = mob_types.pick_random()
+	var mob_random = mob_types.pick_random()
+	if mob_random == "fly":
+		$CollisionShape2D_walk_swim.disabled = true
+		$CollisionShape2D_fly.disabled = false
+	else:
+		
+		$CollisionShape2D_walk_swim.disabled = false
+		$CollisionShape2D_fly.disabled = true
+	$AnimatedSprite2D.animation = mob_random
 	$AnimatedSprite2D.play()
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	queue_free()
+	
